35bd2f337fa4322033aa19b407509b93c22f02b5
dt_zhou <yvchen0921@163.com>
2018-10-11 18:02:52 +0800

    pc端添加商品，登陆完成

--- js/common.js
+++ js/common.js
@@ -1,8 +1,34 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function() {
+    $(this).next().slideToggle();
+});
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+
+// 检测用户是否登录，如果未登录则跳转至登录页
+$.ajax({
+    url: '/api/employee/checkRootLogin',
+    success: function(info) {
+        console.log(info)
+        if (info.error) {
+            location.href = './login.html?url=' + location.href;
+        }
+    }
+});
+
+// 退出登录
+$('.fa-sign-out').on('click', function() {
+
+    // 请求后端接口实现退出登录
+    $.ajax({
+        url: '/api/employee/employeeLogout',
+        success: function(info) {
+            if (info.success) {
+                location.href = '/mobile';
+            }
+        }
+    })
+
+});
